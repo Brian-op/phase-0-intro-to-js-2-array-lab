9e5cbd6f1a74aa2388bb1ab5ad913f2522abcd46
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-22 22:01:14 +0300

    array manipulation

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(_newcat){
+    cats.push('Ralph')}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)};
+
+function destructivelyRemoveLastCat(){
+    cats.pop()};
+
+function destructivelyRemoveFirstCat(){
+cats.shift()}
+
+function appendCat(Broom){
+return [...cats, "Broom"];}
+
+function prependCat(Arnold){
+    return ['Arnold', ...cats]
+}
+
+function removeLastCat(){
+   return cats.slice(0, 2)
+}
+function removeFirstCat(Milo){
+
+    return cats.slice(1);
+}
+
+console.log(cats);
